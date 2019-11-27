9f2ef1bd08b9ea59bf75d991f2ec3ffe7905db33
Jose De Las Salas <jld03e@gmail.com>
2019-11-27 18:25:00 -0500

    updated the second content block and downloaded an additional pic and logos slash icons to add to the parts of the projects that list the tech used for each.

--- portfoliow_react/src/index.js
+++ portfoliow_react/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import JumboHeader from "./JumboHeader";
 import Content from "./Content";
 import amw from "./images/amw.png";
+import MnMTW from "./images/MnMTW.png";
 
 const App = () => {
   return (
@@ -14,9 +15,9 @@ const App = () => {
           <div className="Block1">
             <h3 className="Header1">Affiliate Marketing</h3>
             <p className="para1">
-              Whether you are looking to remodel your current home, or install a
-              sound ground floor on a newly developed home, M & M has the
-              skillset to meet your needs.
+              For whatever items or inventory you are looking to sell, this
+              Affiliate Marketing template allows users to update this site and
+              have their products ready for sale.
             </p>
             <h2 className="Header2">Technology</h2>
             <p className="para2">
@@ -34,7 +35,7 @@ const App = () => {
       <Content>
         <div className="ContentBlock2">
           <div className="Block1">
-            <h3 className="Header1">Affiliate Marketing</h3>
+            <h3 className="Header1">M & M TileWork Site</h3>
             <p className="para1">
               Whether you are looking to remodel your current home, or install a
               sound ground floor on a newly developed home, M & M has the
@@ -42,13 +43,13 @@ const App = () => {
             </p>
             <h2 className="Header2">Technology</h2>
             <p className="para2">
-              CSS for the styling of the site along with the use of SASS to
-              organize and re-use CSS elements throughout the project. Source
-              code for the templates is hosted on Amazon Web Services (AWS).
+              Rendered using React.js, along with CSS for the styling of the
+              site. Using MongoDB, & Express to complete Full-Stack
+              Funcionality, with CRUD.
             </p>
           </div>
           <div className="Block2">
-            <img className="image1" src={amw}></img>
+            <img className="image1" src={MnMTW}></img>
           </div>
         </div>
         <br></br>

9901f6f5dec78a2bde32cd26c8005bee2eabf906
Jose De Las Salas <jld03e@gmail.com>
2020-01-23 14:27:53 -0500

    changed file structure.

---
+++ build/precache-manifest.1b8ace1301905432587e69d8e250c92a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c35bbc5402db95439606df8c06148fd",
+    "url": "/portfolioW_React/index.html"
+  },
+  {
+    "revision": "fe72960850038375363c",
+    "url": "/portfolioW_React/static/css/main.cbb63b4c.chunk.css"
+  },
+  {
+    "revision": "27c4e92983ebfb883c3f",
+    "url": "/portfolioW_React/static/js/2.627e3211.chunk.js"
+  },
+  {
+    "revision": "fe72960850038375363c",
+    "url": "/portfolioW_React/static/js/main.2b8ffd50.chunk.js"
+  },
+  {
+    "revision": "1fba8811a4e9e1bab6a5",
+    "url": "/portfolioW_React/static/js/runtime-main.0fa87400.js"
+  },
+  {
+    "revision": "9f6cef140cbedc42d44bb9cdf490fcc4",
+    "url": "/portfolioW_React/static/media/MnMTW.9f6cef14.png"
+  },
+  {
+    "revision": "1bd393a86e4b61c44a46ff0c994bacf9",
+    "url": "/portfolioW_React/static/media/amw.1bd393a8.png"
+  },
+  {
+    "revision": "215064f1c509f954d9b4a4a0133049ae",
+    "url": "/portfolioW_React/static/media/cards_css.215064f1.png"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/portfolioW_React/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "8d95890abc0bd2749be918a3be47d4df",
+    "url": "/portfolioW_React/static/media/mongoDB.8d95890a.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/portfolioW_React/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "a2bc8ecf6d9052818b36402f0431c60f",
+    "url": "/portfolioW_React/static/media/sass.a2bc8ecf.png"
+  }
+]);
